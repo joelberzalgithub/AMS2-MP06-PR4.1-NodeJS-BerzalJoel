b88e08f12b6ec0a3c8bdf744a82a22d20c877da5
joelberzalgithub <120808626+joelberzalgithub@users.noreply.github.com>
2024-04-28 12:15:14 +0200

    Canviat missatge d'arrencada del servidor

--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 // Inicialitza el servidor, que estarà escoltant en el port 3000
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Escoltant en el port ${port}...`));
+app.listen(port, () => console.log(`Escoltant en el port ${port}...\nBerzal, J.B.A`));
